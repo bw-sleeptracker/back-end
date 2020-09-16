a566fc1b8e1e9e5c999b1a933ca1910891e08653
Jess <jessbonanno@gmail.com>
2020-09-15 22:37:43 -0400

    make it better

--- knexfile.js
+++ knexfile.js
@@ -35,18 +35,20 @@ module.exports = {
     },
   },
 
-  production: {
+production: {
     client: "pg",
-    connection: pgConnection,
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      directory: "./database/migrations",
-    },
-    seeds: {
-      directory: "./database/seeds",
-    },
-  },
+  	useNullAsDefault: true,
+  	connection: {
+		host: `${process.env.DB_HOST}`,
+		user: `${process.env.DB_USER}`,
+		password: `${process.env.DB_PASSWORD}`,
+		database: `${process.env.DB_NAME}`
+  	},
+  	migrations: {
+  		directory: "./database/migrations",
+  	},
+  	seeds: {
+  		directory: "./database/seeds",
+  	}
+  }
 };
