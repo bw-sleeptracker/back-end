42d58b87d3dc19528d216b5c0ba864a1ea167d0e
Jess <jessbonanno@gmail.com>
2020-09-16 13:42:10 -0400

    refactor(/public/index.html): customized welcome title for '/' endopint

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Sleep Tracker API' });
 });
 
 module.exports = router;
