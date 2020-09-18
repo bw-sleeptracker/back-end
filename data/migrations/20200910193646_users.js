4ee42359eb727e84c4abb3f2a3c42d7223f9ea8a
Jess <jessbonanno@gmail.com>
2020-09-17 21:21:29 -0400

    fix(/migrations): added average hours slept to agg data table

--- data/migrations/20200910193646_users.js
+++ data/migrations/20200910193646_users.js
@@ -1,55 +1,56 @@
+exports.up = function (knex) {
+  return knex.schema
 
-exports.up = function(knex) {
-    return knex.schema
+    .createTable("users", tbl => {
+      tbl.uuid('id').notNullable().unique().primary();
+      tbl.string("username", 256).notNullable().unique().index();
+      tbl.string('email', 256).notNullable().unique();
+      tbl.string("password", 256).notNullable();
+      tbl.boolean('admin').defaultTo(false);
+      tbl.double("recommended_hours")
+    })
 
-        .createTable("users", tbl => {
-            tbl.uuid('id').notNullable().unique().primary();
-            tbl.string("username", 256).notNullable().unique().index();
-            tbl.string('email', 256).notNullable().unique();
-            tbl.string("password", 256).notNullable();
-            tbl.boolean('admin').defaultTo(false);
-            tbl.double("recommended_hours")
-        })
+    .createTable("aggregate_data", tbl => {
+      tbl.uuid("id").notNullable().unique().primary()
+      tbl.integer('average_hours_slept');
 
-        .createTable("aggregate_data", tbl =>{
-            tbl.uuid("id").notNullable().unique().primary()
-            tbl.integer("data")
-            tbl.timestamp("week")
-        })
+      tbl.integer("data")
+      tbl.timestamp("week")
+    })
 
-        .createTable("sleep_log", tbl =>{
-            tbl.uuid('id').notNullable().unique().primary();
-            tbl.date("date").notNullable();
-            tbl.text("bedtime").notNullable();
-            tbl.text("wake_time").notNullable();
-            tbl.integer("average_quality").notNullable();
-            tbl.uuid("users_id")
-                .unsigned()
-                .notNullable()
-                .references('id')
-                .inTable("users")
-            tbl.uuid("aggregate_data_id")
-                .unsigned()
-                .notNullable()
-                .references("id")
-                .inTable("aggregate_data")
+    .createTable("sleep_log", tbl => {
+      tbl.uuid('id').notNullable().unique().primary();
+      tbl.date("date").notNullable();
+      tbl.text("bedtime").notNullable();
+      tbl.text("wake_time").notNullable();
+      tbl.integer("average_quality").notNullable();
+      tbl.uuid("users_id")
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable("users")
+      tbl.uuid("aggregate_data_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("aggregate_data")
 
-        })
+    })
 
-        .createTable("quality_log", tbl =>{
-            tbl.uuid("id").notNullable().unique().primary();
-            tbl.integer("wake_score").notNullable();
-            tbl.integer("day_score").notNullable();
-            tbl.integer("bedtime_score").notNullable();
-            tbl.uuid("sleep_log_id")
-                .unsigned()
-                .notNullable()
-                .references("id")
-                .inTable("sleep_log")
-        })
+    .createTable("quality_log", tbl => {
+      tbl.uuid("id").notNullable().unique().primary();
+      tbl.integer("wake_score").notNullable();
+      tbl.integer("day_score").notNullable();
+      tbl.integer("bedtime_score").notNullable();
+      tbl.uuid("sleep_log_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("sleep_log")
+    })
 
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
 
 };
