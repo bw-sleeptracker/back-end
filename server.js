3f69bc1f484641ac72cf0721485cc1dfe0bd20be
Jess <jessbonanno@gmail.com>
2020-09-22 21:06:28 -0400

    cors settings

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const server = express();
 
 server.use(logger('dev'));
 server.use(cors({
-  origin: ['https://sleep-tracker-backend.herokuapp.com'],
+  origin: ['localhost:3000', 'https://sleep-tracker-backend.herokuapp.com'],
   credentials: true,
 }));
 server.use(express.json());
